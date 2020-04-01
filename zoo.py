9ac25dfd90168a9684005cbcb46916e5d7e5ccfc
Luz05 <62961533+Luz05@users.noreply.github.com>
2020-03-31 21:00:10 -0600

    Update zoo.py
    update 1 de zoo

--- zoo.py
+++ zoo.py
@@ -1,29 +1,40 @@
+    
 class animal: 
-    def _init_(self, nombre, tipo):
+    def __init__(self, nombre, tipo):
         self.nombre = nombre
         self.tipo   = tipo
        
+    
     def set_nombre(self,nombre): 
         self.nombre = nombre
+    def set_tipo(self,tipo): 
+        self.tipo = tipo
           
     def get_nombre(self):
         print(self.nombre)
         self.get_miprueba()
-        
+    def get_tipo(self):
+        print(self.nombre)
+        self.get_miprueba()
             
        
         
 def main():
     
     nanimales = int(input("¿Cuántos animales son?: "))
-    lanimales = [nanimales]
-    for x in range(0,nanimales):
+    listanimales = [nanimales]
+    for i in range(nanimales):
         anombre = input("¿Cuál es su nombre?")
         atipo = input("¿Qué tipo es?")
-        if x<(nanimales - 1):
-            lanimales[x] = animal(anombre,atipo)
-
-        
+        if i<(nanimales - 1):
+            listanimales[i] = animal(anombre,atipo)
+        print('Los animales agregados son %s' % listaanimales[:])
+    animbusc=int(input("Dame un id, 0 es el primeroen la lista"))
     
-if _name_ == "_main_":
-	main()
+    posicion=listanimales.index(animbusc)
+    print(posicion)
+    nuevo=(input("nueva categoría"))
+    listanimales.extend(posicion,nuevo)
+    
+    if __name__ == "__main__":
+    main()
